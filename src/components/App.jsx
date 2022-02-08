00a4a8def62d995e2d8119f1f48cb9546cd16a09
Aditya Choudhary <adityachoudhary1980@gmail.com>
2022-02-08 15:40:22 +0530

    Initial

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState,useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  let initNote;
+  if (localStorage.getItem("notes") === null) {
+    initNote = [];
+  } else {
+    initNote = JSON.parse(localStorage.getItem("notes"));
+  }
+  const [notes, setNotes] = useState(initNote);
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  function addNote(newNote) {
+    if (newNote.title && newNote.content !== "") {
+      setNotes((prevNotes) => {
+        return [...prevNotes, newNote];
+      });
+    } else {
+      window.alert("Title or content cannot be empty !");
+    }
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+    
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
